1b28dc20415d062229986a1504c0bcacd19c2c40
Ryan Gittens <ryangittens@hotmail.com>
2015-06-26 18:37:06 -0400

    fixed cross platform compatibility issues (firefox)

--- public/js/modules/parallax.js
+++ public/js/modules/parallax.js
@@ -5,12 +5,16 @@ function scrollFooter(scrollY, heightFooter)
     if(scrollY >= heightFooter)
     {
         $('footer').css({
+             '-ms-transform' : 'translateY(-' + heightFooter + 'px)', 
+             '-webkit-transform' : 'translateY(-' + heightFooter + 'px)',
              'transform' : 'translateY(-' + heightFooter + 'px)'
         });      
     }
     else
     {
         $('footer').css({
+             '-ms-transform' : 'translateY(0)',
+             '-webkit-transform' : 'translateY(0)',
              'transform' : 'translateY(0)'
         });
     }
@@ -22,7 +26,6 @@ $(window).load(function(){
         footerHeight        = $('footer').outerHeight(),
         heightDocument      = ($('.all-content').offset().top) + ($('.all-content').outerHeight()) + ($('footer').outerHeight()) - 20;
     // Size the parallax container (should be content plus footer plus fixed header..)
-    console.log(heightDocument)
     $('#scroll-animate-main, #scroll-animate').css({
         'height' :  heightDocument + 'px'
     });
@@ -34,6 +37,8 @@ $(window).load(function(){
         var scroll = window.scrollY;
 
         $('#scroll-animate-main').css({
+            '-ms-transform' : 'translateY(-' + scroll + 'px)',
+            '-webkit-transform' : 'translateY(-' + scroll + 'px)',
             'transform' : 'translateY(-' + scroll + 'px)'
         });
 
